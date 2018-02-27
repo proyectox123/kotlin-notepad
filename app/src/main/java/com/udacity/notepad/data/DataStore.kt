452bac5a78482a299b89daa0d17efd8f396dac37
MHO <mauricio.hdez.o.10@gmail.com>
2018-02-26 18:39:35 -0600

    Converted NotesOpenHelper to Kotlin.
    Implemented JvmStatic annotation.
    
    Using high-order function.

---
+++ app/src/main/java/com/udacity/notepad/data/DataStore.kt
@@ -0,0 +1,26 @@
+package com.udacity.notepad.data
+
+import android.content.Context
+import org.jetbrains.anko.doAsync
+
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+object DataStore {
+
+    @JvmStatic
+    lateinit var notes: NoteDatabase
+        private set
+
+    fun init(context: Context) {
+        notes = NoteDatabase(context)
+    }
+
+    fun execute(runnable: Runnable) {
+        execute { runnable.run() }
+    }
+
+    fun execute(fn: () -> Unit){
+        doAsync { fn() }
+    }
+}
